c8af5e2b55a9d94d2bd9748523e8beffa9bf51e0
Luigi <luigi.colombo@smartodds.co.uk>
2016-09-27 14:12:04 +0100

    Add inline initialisation

--- src/number.cpp
+++ src/number.cpp
@@ -7,10 +7,7 @@ using namespace cppcourse;
 
 int main () {
 
-    Number n1(2);
-
-    Number n2(3), n6;
-    Number n3a;
+    Number n1(2), n2(3), n6;
     Number n3 = n1 + n2;
     Number n4 = n1 + 1;
     Number n5 = 10 + n2;
