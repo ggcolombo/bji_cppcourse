5a08582d597ea519ac57801ab185e106afee69a4
Luigi <luigi.colombo@smartodds.co.uk>
2016-09-27 16:47:41 +0100

    Add memoized local square function.

--- src/square.cpp
+++ src/square.cpp
@@ -4,6 +4,8 @@
 #include "square.h"
 #include "memoize.h"
 
+double square2 (double n) {return n*n;}
+
 int main () {
 	
 	cppcourse::Square<double> s;
@@ -13,6 +15,10 @@ int main () {
 	std::cout << s.get(4) << std::endl;
 	std::cout << s.get(-4) << std::endl;
 	std::cout << s.get(5) << std::endl;
+
+    auto square2m = memo<double, double>(square2);
+    std::cout << square2m(4) << std::endl;
+    std::cout << square2m(4) << std::endl;
 
 	return 0;
 }
