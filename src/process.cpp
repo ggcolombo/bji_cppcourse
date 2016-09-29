f2cd8b5a32f1d1b38beeede5612f7129e7570e2f
Luigi <luigi.colombo@gmail.com>
2016-09-29 12:13:04 +0100

    Add vector of handles;

--- src/process.cpp
+++ src/process.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <vector>
+
 using namespace std;
 
 #include "stringprocess.h"
@@ -15,6 +17,13 @@ int main()
 	ProcessHandle ph(std::make_shared<StringProcess>("Greetings, "));
 	auto msg2 = ph.doProcess(65);
 	cout << msg2 << endl;
+
+	vector<ProcessHandle> handles;
+	handles.push_back(ProcessHandle(make_shared<StringProcess>("one == ")));
+	handles.push_back(ProcessHandle(make_shared<StringProcess>("two != ")));
+
+	for (auto h : handles)
+		cout << h.doProcess(1) << endl;
 
 	return 0;
 }
